05968fb41dd8fec6fd085581ac15d8a76228d143
Harsh Wardhan <harshwardhan847@gmail.com>
2025-07-08 11:59:51 +0530

    minor

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,14 @@
 import type { Route } from "./+types/home";
-import AudioChat from "../welcome/welcome";
+import AudioChat from "../audio_chat";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kisan Mitra" },
+    {
+      name: "description",
+      content:
+        "Leveraging Google's AI stack to create an intuitive, voice-first intelligent agent, providing farmers with an omnipresent expert for precise crop care, market foresight, and seamless government scheme guidance.",
+    },
   ];
 }
 
